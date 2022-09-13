19ac683260e0331b60d5c63337f52c4d1781646e
Kerengarba <kerenhappuchgarba@gmail.com>
2022-09-12 20:09:09 -0700

    j

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,8 +8,9 @@
 int main(void)
 {	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++'\n')
-		putchar(ch);
-	
+	for (ch = 'a'; ch <= 'z'; ch++)	
+	{	putchar(ch);
+	        putchar('\n');
+		}
 return (0);
 }
