a0e904cf07f01b17cbabe9073335381980208a9a
Kerengarba <kerenhappuchgarba@gmail.com>
2022-09-14 17:34:12 -0700

    g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,9 @@
  * Return: Always 0 Success
  */
 
-void print(char *a);
-
 int main(void)
-	{
-		char a[] = "_putchar\n";
+{	
+	printf("_putchar\n");
 
 return (0);
-}
-void print(char *a)
-{
-	int i = 0;
-	(putchar(s[i]);
-	i++
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+int void print_alphabet(void);
+int main(void)
+{	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		
+	{	putchar(ch);
+		}
+
+return (0);
+}
