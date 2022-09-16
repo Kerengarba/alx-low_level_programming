0ecc6a9b45a6ef7b39f5430fbb2b2ad5606f809e
Kerengarba <kerenhappuchgarba@gmail.com>
2022-09-15 21:32:24 -0700

    f

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - check the code
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+
+{
+	int i = 0;
+
+	while (i<=0)
+		printf("i++");
+return (0);
+		
+}
+		
