383327bd54378a56a64cb49657a8e0abb3a17bb1
Kerengarba <kerenhappuchgarba@gmail.com>
2022-09-15 20:53:38 -0700

    g

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+	printf("100352\n");
+	printf("-1646592\n");	
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int isupper(int c);
 int isdigit(int c);
-int mul(int a,  int b);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
