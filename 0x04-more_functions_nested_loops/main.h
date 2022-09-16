d22ea6c40fdd96d29255f47dc70f370082bc130c
Kerengarba <kerenhappuchgarba@gmail.com>
2022-09-15 19:03:53 -0700

    digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 int isupper(int c);
-int _isdigit(int c);
+int isdigit(int c);
 int mul(int a,  int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -8,4 +8,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int char(character);
+int putchar(int character);
