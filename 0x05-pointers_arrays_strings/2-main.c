00c230a5798df506697deb47b622965ede140b8e
Kerengarba <kerenhappuchgarba@gmail.com>
2022-09-20 20:52:25 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - to check the string length of a string using strlen
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	int len;
+
+	len = 16;
+	printf("%d\n, len");
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int _strlen function is to print the string length of the string.
+ *
+ * @*s: sets the string to be printed of it length.
+ *
+ * Return: Always 0
+ */
+int _strlen(char *str)
+{
+	*str = 16;
+}
+
