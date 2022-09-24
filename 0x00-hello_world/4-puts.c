de568cdbc638002840bfab7393812fe4a1010991
Kerengarba <kerenhappuchgarba@gmail.com>
2022-09-23 17:18:09 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/** 
+/**
  * main - function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char str[98] = "Programming is like building a multilingual puzzle\n";
+	char str[98] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(str);
 	return (0);
 }
